64c47af05f080f1844da1e99068a11f7dc713ae7
DailySunnyOli <56804970+DailySunnyOli@users.noreply.github.com>
2022-06-20 20:12:20 -0300

    (feat) cociente
    creación de clase y dos métodos estáticos

---
+++ Cociente.py
@@ -0,0 +1,8 @@
+class Cociente:
+    @staticmethod
+    def cocienteEntero(valor1, valor2):
+        return valor1//valor2
+
+    @staticmethod
+    def cocienteFloat(valor1, valor2):
+        return valor1/valor2
