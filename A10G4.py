81e942350e2b3d29cdee674f7c4b2644bfc9d8a2
Gabriel Paz <fgpaz@live.com>
2022-06-20 19:02:08 -0300

    (fix): arreglo de nombre

--- A10G4.py
+++ A10G4.py
@@ -1,7 +1,8 @@
+from unittest import result
 from ing2i import IngresoEnteros
 from ing2s import IngresoStrings
 
-if "__main__" == __name__:
+if "A10G4" == __name__:
     print('-----------------\n|    GRUPO 4    |\n-----------------\n')
     print('Inicio de Situacion Profesional 1:')
     entero1, entero2 = IngresoEnteros.ing2i()
