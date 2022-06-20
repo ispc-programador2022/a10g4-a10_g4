397e9c0667d3ac1232136d257cfa271bebc5ffb9
Gabriel Paz <fgpaz@live.com>
2022-06-20 14:04:54 -0300

    First Commit. Completada primera issue

---
+++ ing2i.py
@@ -0,0 +1,6 @@
+class IngresoEnteros:
+    @staticmethod
+    def ing2i():
+        entero1 = int(input('Ingrese valor entero 1:'))
+        entero2 = int(input('Ingrese valor entero 2:'))
+        return entero1, entero2
---
+++ main.py
@@ -0,0 +1,6 @@
+from ing2i import IngresoEnteros
+from ing2s import IngresoStrings
+
+if "__main__" == __name__:
+    entero1, entero2 = IngresoEnteros.ing2i()
+    string1, string2 = IngresoStrings.ing2s()
