7b671433dc2827b2b8b40b244c3e2188354aa132
DailySunnyOli <56804970+DailySunnyOli@users.noreply.github.com>
2022-06-22 20:11:31 -0300

    (fit): arreglo de main
    borrado de llamados a clases próximas a ser eliminadas

--- A10G4.py
+++ A10G4.py
@@ -1,8 +1,6 @@
 from unittest import result
 from ing2i import IngresoEnteros
 from ing2s import IngresoStrings
-from Producto import Producto
-from Cociente import Cociente
 from Cociente2P import CocienteDosParametros
 from Modulo import ModuloDosParametros
 from Producto2P import ProductoDosParametros
@@ -12,13 +10,6 @@ if "A10G4" == __name__:
     print('Inicio de Situacion Profesional 1:')
     entero1, entero2 = IngresoEnteros.ing2i()
     string1, string2 = IngresoStrings.ing2s()
-
-    #US2- Ejercicios Producto y Cociente
-
-    producto = Producto.producto(entero1, entero2)
-    if (entero2 != 0):
-        cocienteInt = Cociente.cocienteEntero(entero1, entero2)
-        cocienteFloat = Cociente.cocienteFloat(entero1, entero2)
 
     #US3 - Ejercicios Producto, cociente y modulo
     if (entero2 != 0):
