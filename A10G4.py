c54a95eb11b69c286cd9063f53dffd2dbd105cbf
DailySunnyOli <56804970+DailySunnyOli@users.noreply.github.com>
2022-06-22 20:21:11 -0300

    (fit): arreglo main inclusión suma y resta
    se corrigió el main para incluir las funciones de suma y resta

--- A10G4.py
+++ A10G4.py
@@ -4,12 +4,18 @@ from ing2s import IngresoStrings
 from Cociente2P import CocienteDosParametros
 from Modulo import ModuloDosParametros
 from Producto2P import ProductoDosParametros
+from Suma import Suma
+from Resta import Resta
 
 if "A10G4" == __name__:
     print('-----------------\n|    GRUPO 4    |\n-----------------\n')
     print('Inicio de Situacion Profesional 1:')
     entero1, entero2 = IngresoEnteros.ing2i()
     string1, string2 = IngresoStrings.ing2s()
+
+    #US2 - Ejercicios Suma y Resta
+    print("La suma de los dos valores ingresados es: ", Suma.suma(entero1, entero2))
+    print("La resta de los dos valores ingresados es: ", Resta.resta(entero1, entero2))
 
     #US3 - Ejercicios Producto, cociente y modulo
     if (entero2 != 0):
